ef33a21a4c71a318d624e282f4d9e5b54e4fdad5
aromansky <kirbi.zsthan@gmail.com>
2023-12-10 22:54:39 +0300

    Поломал проект

--- SoftwareEngineering/SoftwareEngineering.cs
+++ SoftwareEngineering/SoftwareEngineering.cs
@@ -1,11 +1,9 @@
-﻿using static Funcs.MyFuncs;
-using static Funcs.RecurHTask;
-
+﻿
 namespace SoftwareEngineering
 {
     internal class SoftwareEngineering
     {
-        static void Main()
+        //static void Main()
         {
 
         }
